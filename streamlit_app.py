c99bc0e1568c3e3627355aab06ea6588de4ce1bb
Fahim Qureshi <120321138+fahimq19241@users.noreply.github.com>
2022-12-18 21:35:20 +0000

    Update streamlit_app.py
    added try / except clause

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-# import pandas
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -33,20 +33,29 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # takes the JSON version of the data and normalises it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # outputs to the screen
+    streamlit.dataframe(fruityvice_normalized)
+  
+  streamlit.write('The user entered ', fruit_choice)
 
-# import requests
+except URLError as e:
+  streamlit.error()
 
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-# streamlit.text(fruityvice_response.json()) #This just wrote the data to the screen
 
-# takes the JSON version of the data and normalises it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# outputs to the screen
-streamlit.dataframe(fruityvice_normalized)
+
+
+
+
+
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
@@ -54,7 +63,7 @@ streamlit.stop()
 
 
 
-# import snowflake.connector
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
